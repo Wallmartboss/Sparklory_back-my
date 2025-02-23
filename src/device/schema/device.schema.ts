de3f6d8ce36a68db6fcef7a584ac62735f01539a
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2025-02-23 14:27:16 +0200

    add versionKey false

--- src/device/schema/device.schema.ts
+++ src/device/schema/device.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
 import { User } from 'src/user/schema/user.schema';
 
-@Schema({ collection: 'device' })
+@Schema({ collection: 'device', versionKey: false })
 export class Device extends Document {
   @Prop({ required: true })
   deviceId: string;
--- src/session/schema/session.schema.ts
+++ src/session/schema/session.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { Document } from 'mongoose';
 import { User } from 'src/user/schema/user.schema';
 
-@Schema({ collection: 'session' })
+@Schema({ collection: 'session', versionKey: false })
 export class Session extends Document {
   @Prop({ required: true, default: () => new Date().toISOString() })
   createdAt: string;
