408a2509c95b9b4b3dd4138e49b11dd1af94fc2f
Yuri <wall.mart.boss@proton.me>
2025-07-15 19:04:42 +0300

    upd Swagger documentation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -27,6 +27,27 @@ export class UserController {
   @ApiOperation({
     summary: 'view your own profile',
   })
+  @ApiResponse({
+    status: 200,
+    description: 'User profile',
+    schema: {
+      example: {
+        _id: '67c73504f555f43ff7a6ab8e',
+        email: 'wall.mart.boss@proton.me',
+        name: 'Wall Mart Boss',
+        role: 'user',
+        image: 'https://example.com/image.jpg',
+        isLoggedIn: true,
+        isVerifyEmail: false,
+        createdAt: '2025-03-04T17:14:44.832Z',
+        updatedAt: '2025-03-04T17:14:44.833Z',
+        wishlist: ['60d21b4667d0d8992e610c85', '60d21b4967d0d8992e610c86'],
+        facebookId: '1234567890',
+        googleId: 'abcdefg123456',
+        resetPasswordCode: '123456',
+      },
+    },
+  })
   @Get('me')
   async me(@UserDecorator('sub') sub: string) {
     return this.userService.me(sub);
@@ -38,7 +59,27 @@ export class UserController {
   @Post('wishlist/:productId')
   @ApiOperation({ summary: 'Add product to wishlist' })
   @ApiParam({ name: 'productId', type: String })
-  @ApiResponse({ status: 200, description: 'Product added to wishlist' })
+  @ApiResponse({
+    status: 200,
+    description: 'Product added to wishlist',
+    schema: {
+      example: {
+        _id: '67c73504f555f43ff7a6ab8e',
+        email: 'wall.mart.boss@proton.me',
+        name: 'Wall Mart Boss',
+        role: 'user',
+        image: 'https://example.com/image.jpg',
+        isLoggedIn: true,
+        isVerifyEmail: false,
+        createdAt: '2025-03-04T17:14:44.832Z',
+        updatedAt: '2025-03-04T17:14:44.833Z',
+        wishlist: ['60d21b4667d0d8992e610c85', '60d21b4967d0d8992e610c86'],
+        facebookId: '1234567890',
+        googleId: 'abcdefg123456',
+        resetPasswordCode: '123456',
+      },
+    },
+  })
   async addToWishlist(
     @UserDecorator('sub') sub: string,
     @Param('productId') productId: string,
@@ -51,7 +92,56 @@ export class UserController {
    */
   @Get('wishlist')
   @ApiOperation({ summary: 'Get all products from wishlist' })
-  @ApiResponse({ status: 200, description: 'List of products in wishlist' })
+  @ApiResponse({
+    status: 200,
+    description: 'List of products in wishlist',
+    schema: {
+      example: [
+        {
+          _id: '60f7c2b8e1d2c8001c8e4c1a',
+          name: 'Amethyst Earrings',
+          description: 'Earrings with white gold and amethysts',
+          category: 'earrings',
+          engraving: true,
+          image: ['12345678.jpg'],
+          action: ['Spring sale'],
+          prod_collection: 'Spring 2025',
+          discount: 30,
+          discountStart: '2025-07-10T00:00:00.000Z',
+          discountEnd: '2025-07-20T23:59:59.000Z',
+          subcategory: ['casual', 'sport'],
+          gender: 'unisex',
+          details: ['Handmade', '925 Silver', 'Gift box included'],
+          reviews: [
+            {
+              name: 'Ivan',
+              avatar: 'avatar123.jpg',
+              text: 'Great product!',
+              rating: 5,
+              createdAt: '20.10.2024',
+              image: ['reviewImage1.jpg'],
+            },
+          ],
+          variants: [
+            {
+              material: 'silver',
+              size: 'L',
+              price: 8000,
+              insert: 'Silver',
+              inStock: 7,
+            },
+            {
+              material: 'white gold',
+              size: 'M',
+              price: 12200,
+              insert: 'White Gold',
+              inStock: 5,
+            },
+          ],
+        },
+      ],
+    },
+  })
   async getWishlist(@UserDecorator('sub') sub: string) {
     return this.userService.getWishlist(sub);
   }
